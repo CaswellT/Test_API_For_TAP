c07b241584fc619b8a94e98209764aa34eeb301a
Caswell Taylor <caswell.taylor@ttu.edu>
2024-01-08 11:45:05 -0600

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_API_For_TAP.Models;
+
+namespace Test_API_For_TAP.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet, Route("Get-Message")]
+        public IActionResult GetMessage()
+        {
+            UserInfo user = new UserInfo { FirstName = "John", LastName = "Eads" };
+
+
+
+            return Ok($"{user.FirstName} {user.LastName} can suck it");
+        }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Test_API_For_TAP.Models
+{
+    public class UserInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
